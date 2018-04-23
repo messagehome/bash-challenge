a0c63c51278c7d64e7867ec499f07829e47bc0f5
messagehome <38570859+messagehome@users.noreply.github.com>
2018-04-23 08:09:54 -0400

    Add files via upload

--- my-script.sh
+++ my-script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# use grep to find actors and remove headers in users.csv, then sort column 2 in reverse alphabetical, finish by displaying only rows FirstName LastName <email> 
+# use grep to find actors and remove headers in users.csv, then use sort on column 2 in reverse alphabetical, finish by displaying only rows FirstName LastName <email> using awk
 grep "actor" users.csv | sort -k2 -r -t, | awk 'BEGIN {FS=OFS=","} {print $4,$5,$3}'
